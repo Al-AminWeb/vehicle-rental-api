7a8f68be72da6e6df8fcd16630b56c93482220c2
Al-AminWeb <mdalaminweb.1@gmail.com>
2025-12-11 01:19:48 +0600

    user.routes added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, {NextFunction, Request, Response} from "express";
 import config from "./config";
 import initDB, {pool} from "./config/db";
+import {userRoutes} from "./modules/users/user.routes";
 
 
 
@@ -24,20 +25,10 @@ app.get("/", (req:Request, res: Response) => {
 });
 
 //user crud
-app.post("/users", async (req: Request, res: Response) => {
-    const {name, email, password, phone, role} = req.body;
-    try {
-const result = await pool.query(`INSERT INTO users (name, email, password, phone, role) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [name, email, password, phone, role]);
-        res.status(201).json({
-            success:false,
-            message:"User created successfully",
-            data:result.rows[0],
-        });
-    } catch (err: any) {
-        res.status(400).send(err.message);
-    }
 
-})
+
+app.use("/users",userRoutes)
+
 
 app.get("/users", async (req: Request, res: Response) => {
     try {
