c9a57abbb7653b8169301c7f35f472aa95ddb46d
Al-AminWeb <mdalaminweb.1@gmail.com>
2025-12-11 00:49:15 +0600

    new db configaration added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, {NextFunction, Request, Response} from "express";
-import {Pool} from "pg";
 import config from "./config";
+import initDB, {pool} from "./config/db";
 
 
 
@@ -8,48 +8,7 @@ const app = express();
 const port = config.port;
 app.use(express.json());
 
-const pool = new Pool({
-    connectionString: `${config.connection_str}`
-});
 
-const initDB = async () => {
-    // USERS TABLE
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(150) NOT NULL UNIQUE,
-            password TEXT NOT NULL,
-            phone VARCHAR(20) NOT NULL,
-            role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer'))
-        );
-    `);
-
-    // VEHICLES TABLE
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS vehicles (
-            id SERIAL PRIMARY KEY,
-            vehicle_name VARCHAR(150) NOT NULL,
-            type VARCHAR(20) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
-            registration_number VARCHAR(50) NOT NULL UNIQUE,
-            daily_rent_price NUMERIC NOT NULL CHECK (daily_rent_price > 0),
-            availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked'))
-        );
-    `);
-
-    // BOOKINGS TABLE
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS bookings (
-            id SERIAL PRIMARY KEY,
-            customer_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-            vehicle_id INTEGER NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
-            rent_start_date DATE NOT NULL,
-            rent_end_date DATE NOT NULL,
-            total_price NUMERIC NOT NULL CHECK (total_price > 0),
-            status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned'))
-        );
-    `);
-};
 
 initDB();
 
